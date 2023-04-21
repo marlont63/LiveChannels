c5943275e4ab5a1e236f5277b194723b3fd41ff9
marlont63 <tavarez.marlon@gmail.com>
2023-04-21 07:35:23 +0200

    add gitignore

---
+++ LiveChannels/LiveChannels/LiveChannelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveChannelsApp.swift
+//  LiveChannels
+//
+//  Created by Marlon Raschid Tavarez Parra on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveChannelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
